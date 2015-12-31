3ba9ae3844205b72f605da7a1244898e739f471c
Khuat Viet Hoa <hoa.khuatviet@gmail.com>
2015-12-31 08:41:25 +0700

    initialise

--- app/build/generated/source/r/debug/com/netcompss/ffmpeg4android/R.java
+++ app/build/generated/source/r/debug/com/netcompss/ffmpeg4android/R.java
@@ -11,25 +11,25 @@ public final class R {
     public static final class attr {
     }
     public static final class id {
-        public static int action_about=0x7f050000;
+        public static final int action_about=0x7f050000;
     }
     public static final class menu {
-        public static int main=0x7f040000;
+        public static final int main=0x7f040000;
     }
     public static final class string {
-        public static int app_name=0x7f020000;
-        public static int commandText=0x7f020001;
-        public static int commandTextView=0x7f020002;
-        public static int email=0x7f020003;
-        public static int full_app_name=0x7f020004;
-        public static int welcome_1=0x7f020005;
-        public static int welcome_2=0x7f020006;
-        public static int welcome_3=0x7f020007;
-        public static int welcome_demo=0x7f020008;
+        public static final int app_name=0x7f020000;
+        public static final int commandText=0x7f020001;
+        public static final int commandTextView=0x7f020002;
+        public static final int email=0x7f020003;
+        public static final int full_app_name=0x7f020004;
+        public static final int welcome_1=0x7f020005;
+        public static final int welcome_2=0x7f020006;
+        public static final int welcome_3=0x7f020007;
+        public static final int welcome_demo=0x7f020008;
     }
     public static final class style {
-        public static int h1=0x7f030000;
-        public static int h2=0x7f030001;
-        public static int t1=0x7f030002;
+        public static final int h1=0x7f030000;
+        public static final int h2=0x7f030001;
+        public static final int t1=0x7f030002;
     }
 }
